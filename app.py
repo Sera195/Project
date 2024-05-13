9f4451cf85de1e5a7abb514fc99b960c6b67bc0d
Sera195 <163730754+Sera195@users.noreply.github.com>
2024-05-13 10:31:30 +0200

    test

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import requests
 import streamlit as st
 
+st.write("HELLO")
 
 headers = {
-    "authorization": st.secrets["auth_token"],
+    "authorization": st.secrets["auth_key"],
     "content-type": "application/json"
 }
