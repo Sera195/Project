50a6c711c1bbfcf5f489f102862869ae778ff16b
Sera195 <163730754+Sera195@users.noreply.github.com>
2024-05-13 11:13:37 +0200

    sdf

--- app.py
+++ app.py
@@ -1,23 +1,10 @@
 import requests
 import streamlit as st
 import pydeck as pdk
+import pandas as pd
 
-layer = pdk.Layer(
-    'ScatterplotLayer',
-    data=df, # your dataframe
-    get_position='[lon, lat]',
-    get_color='[200, 30, 0, 160]',
-    get_radius=200,
-)
-
-st.pydeck_chart(pdk.Deck(
-    map_style='mapbox://styles/mapbox/light-v9',
-    initial_view_state=pdk.ViewState(
-        latitude=37.76,
-        longitude=-122.4,
-        zoom=11,
-        pitch=50,
-    ),
-    layers=[layer],
-    api_keys={'mapbox': 'your_mapbox_api_key', 'google_maps': 'auth_key'},
-))
+data = pd.DataFrame({
+    'lat': [37.76, 34.05],
+    'lon': [-122.4, -118.25]
+})
+st.map(data)
