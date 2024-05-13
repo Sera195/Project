13be23c177d56b6c50d54d9bdc877b4afe4f8b00
Sera195 <163730754+Sera195@users.noreply.github.com>
2024-05-13 11:14:33 +0200

    sdf

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ import requests
 import streamlit as st
 import pydeck as pdk
 import pandas as pd
+import folium
+from streamlit_folium import st_folium
+
+m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+st_data = st_folium(m, width=725)
 
 data = pd.DataFrame({
     'lat': [37.76, 34.05],
