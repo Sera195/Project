c7ffd3e92dad07b88346f2a94860be76001fae18
Sera195 <163730754+Sera195@users.noreply.github.com>
2024-05-13 11:55:10 +0200

    sdf

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ def get_train_route(api_key, start_location, end_location):
             departure_time = step['transit_details']['departure_time']['text']
             arrival_time = step['transit_details']['arrival_time']['text']
             duration = step['duration']['text']
-            route_coordinates.append([step['start_location']['lng'], step['start_location']['lat']])
-            route_coordinates.append([step['end_location']['lng'], step['end_location']['lat']])
+            route_coordinates.append([step['start_location']['lat'], step['start_location']['lng']])
+            route_coordinates.append([step['end_location']['lat'], step['end_location']['lng']])
             processed_data.append({
                 'departure_station': departure_station,
                 'arrival_station': arrival_station,
