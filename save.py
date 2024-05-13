3e25921c0d7f7bfadcbc1af566d7dd688084312a
Sera195 <163730754+Sera195@users.noreply.github.com>
2024-05-13 13:47:42 +0200

    sdf

--- save.py
+++ save.py
@@ -3,37 +3,38 @@ import googlemaps
 import pandas as pd
 import requests
 
-# Funktion zum Abrufen der Zugroute von Google Maps API
+# Funktion zum Abrufen der Zugroute von Google Maps API mit einem GET-Request
 def get_train_route(api_key, start_location, end_location):
-    # Initialisiere Google Maps Client
-    gmaps = googlemaps.Client(key=api_key)
+    url = f"https://maps.googleapis.com/maps/api/directions/json?origin={start_location}&destination={end_location}&mode=transit&transit_mode=rail&key={api_key}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        # Verarbeite die Daten und extrahiere relevante Informationen
+        if "routes" in data and len(data["routes"]) > 0:
+            steps = data["routes"][0]["legs"][0]["steps"]
+            processed_data = []
+            route_coordinates = []
+            for step in steps:
+                if step['travel_mode'] == 'TRANSIT':
+                    departure_station = step['transit_details']['departure_stop']['name']
+                    arrival_station = step['transit_details']['arrival_stop']['name']
+                    line = step['transit_details'].get('line', {}).get('name', "Unknown")
+                    departure_time = step['transit_details']['departure_time']['text']
+                    arrival_time = step['transit_details']['arrival_time']['text']
+                    duration = step['duration']['text']
+                    route_coordinates.append((step['start_location']['lat'], step['start_location']['lng']))
+                    route_coordinates.append((step['end_location']['lat'], step['end_location']['lng']))
+                    processed_data.append({
+                        'departure_station': departure_station,
+                        'arrival_station': arrival_station,
+                        'line': line,
+                        'departure_time': departure_time,
+                        'arrival_time': arrival_time,
+                        'duration': duration
+                    })
 
-    # Abfrage für die Zugroute
-    train_route = gmaps.directions(start_location, end_location, mode="transit", transit_mode="rail")
-
-    # Verarbeite die Daten und extrahiere relevante Informationen
-    processed_data = []
-    route_coordinates = []
-    for step in train_route[0]['legs'][0]['steps']:
-        if step['travel_mode'] == 'TRANSIT':
-            departure_station = step['transit_details']['departure_stop']['name']
-            arrival_station = step['transit_details']['arrival_stop']['name']
-            line = step['transit_details'].get('line', {}).get('name', "Unknown")
-            departure_time = step['transit_details']['departure_time']['text']
-            arrival_time = step['transit_details']['arrival_time']['text']
-            duration = step['duration']['text']
-            route_coordinates.append((step['start_location']['lat'], step['start_location']['lng']))
-            route_coordinates.append((step['end_location']['lat'], step['end_location']['lng']))
-            processed_data.append({
-                'departure_station': departure_station,
-                'arrival_station': arrival_station,
-                'line': line,
-                'departure_time': departure_time,
-                'arrival_time': arrival_time,
-                'duration': duration
-            })
-
-    return pd.DataFrame(processed_data), route_coordinates
+            return pd.DataFrame(processed_data), route_coordinates
+    return None, None
 
 # Funktion zur Umwandlung von Ortsnamen in Koordinaten
 def get_coordinates(place, api_key):
@@ -69,13 +70,16 @@ def main():
             # Rufe die Zugroute und die Koordinaten ab
             train_route, route_coordinates = get_train_route(api_key, f"{start_lat},{start_lng}", f"{end_lat},{end_lng}")
             
-            # Zeige die Zugroute als Tabelle an
-            st.subheader(f"Zugroute von {start_location} nach {end_location}")
-            st.write(train_route)
+            if train_route is not None:
+                # Zeige die Zugroute als Tabelle an
+                st.subheader(f"Zugroute von {start_location} nach {end_location}")
+                st.write(train_route)
 
-            # Erstelle eine Google Maps-Karte für die Zugroute
-            st.subheader(f"Zugroute von {start_location} nach {end_location} auf Karte anzeigen")
-            st.markdown(f'<iframe width="100%" height="500" src="https://www.google.com/maps/embed/v1/directions?key={api_key}&origin={start_lat},{start_lng}&destination={end_lat},{end_lng}&mode=transit" allowfullscreen></iframe>', unsafe_allow_html=True)
+                # Erstelle eine Google Maps-Karte für die Zugroute
+                st.subheader(f"Zugroute von {start_location} nach {end_location} auf Karte anzeigen")
+                st.markdown(f'<iframe width="100%" height="500" src="https://www.google.com/maps/embed/v1/directions?key={api_key}&origin={start_lat},{start_lng}&destination={end_lat},{end_lng}&mode=transit" allowfullscreen></iframe>', unsafe_allow_html=True)
+            else:
+                st.warning("Keine Route gefunden.")
     else:
         st.warning("Bitte geben Sie Ihren Google Maps API-Schlüssel ein und stellen Sie sicher, dass die Start- und Zielorte gültig sind.")
 
