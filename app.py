b239b103f761574f815d0630a26e4498e3ff33b6
Sera195 <163730754+Sera195@users.noreply.github.com>
2024-05-13 10:33:18 +0200

    test

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import requests
 import streamlit as st
+import gmaps
 
 st.write("HELLO, im asdlfkj")
 
@@ -7,3 +8,14 @@ headers = {
     "authorization": st.secrets["auth_key"],
     "content-type": "application/json"
 }
+
+def get_coordinates(place):
+    url = f"https://maps.googleapis.com/maps/api/geocode/json?address={place}&key={"auth_key"}"
+    response = requests.get(url)
+    #überprüfen ob code == 200 (Verbindung OK)
+    if response.status_code == 200:
+        data = response.json()
+        if "results" in data and len(data["results"]) > 0:
+            location = data["results"][0]["geometry"]["location"]
+            return location["lat"], location["lng"]
+    return None, None
